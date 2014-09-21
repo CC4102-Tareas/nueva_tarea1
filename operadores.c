4943c197568e5bc759cddf6a0b87d1ef18e389ce
Felipe Hernández <cephei.1313@gmail.com>
2014-09-21 10:05:37 -0300

    se agregan comentarios.

--- operadores.c
+++ operadores.c
@@ -1,5 +1,6 @@
 #define TRUE 1
 #define FALSE 0
+
 /**
 	Procedimiento basado en http://stackoverflow.com/questions/13390333/two-rectangles-intersection	
 	verifica si existe una intersección entre 2 rectángulos dados.
@@ -15,13 +16,12 @@ float flmax(float a, float b)
 	return (((a) > (b)) ? (a) : (b));
 }
 
-
 int interseccion (Rectangulo r1, Rectangulo r2) 
 {
 	if  (r2.x1 >= r1.x2 || 
-		  r2.x2 <= r1.x1 || 
-		  r2.y2 <= r1.y1 ||
-		  r2.y1 >= r1.y2)
+		 r2.x2 <= r1.x1 || 
+		 r2.y2 <= r1.y1 ||
+		 r2.y1 >= r1.y2)
 		return 0;
 	return 1;
 }
@@ -55,7 +55,6 @@ float incremento_area(Rectangulo r1, Rectangulo r2) {
 	Rectangulo mbrect = calculate_mbr(r1,r2);
 	return area(mbrect) - area(r1);
 }
-
 
 float incremento_area_qs(Rectangulo r1, Rectangulo r2) {
 	Rectangulo mbrect = calculate_mbr(r1,r2);
