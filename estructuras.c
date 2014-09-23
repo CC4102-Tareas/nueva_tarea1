037c41be3169de7f67c6277899cf8fb12679275e
choreka ruben <rclaveria89@gmail.com>
2014-09-22 22:14:17 -0300

    cambia T de la estructura

--- estructuras.c
+++ estructuras.c
@@ -1,5 +1,5 @@
 #define TAMANO_PAGINA 4096 // tamaño de página del disco duro.
-#define T 2                // mínimo de hijos que puede tener un nodo (salvo la raíz)
+#define T 102              // mínimo de hijos que puede tener un nodo (salvo la raíz)
 
 int rtree_ultimo = -1; // almacena el último identificador formado.
 int rtree_root = -1;   // almacena el identificador de la raíz
@@ -51,9 +51,9 @@ typedef struct
 	int nodo_padre;   	// posición del nodo padre en el archivo.
 	int pos_mbr_padre;	// posición del MBR padre en el nodo padre(índice del arreglo MBR).
 	int ultimo;       	// último indice ocupado en mbr.
-	MBR mbr[4];			// arreglo de MBR's. Equivalente cuando T=2.
-	MBR mbr_[2*102-4];	// arreglo de MBR's relleno
-	//MBR mbr[2*T];	      // arreglo de MBR's
+	//MBR mbr[4];			// arreglo de MBR's. Equivalente cuando T=2.
+	//MBR mbr_[2*102-4];	// arreglo de MBR's relleno
+	MBR mbr[2*T];	      // arreglo de MBR's
 } Nodo;
 
 // utilizado para retornar los nodos creados en el split
